fb21a97ab2a4c6fcee0af3ce4a334cb6c3ecff93
hemanthwtw <hemanthwtw@gmail.com>
2026-05-08 15:06:55 +0530

    feat: add test script

---
+++ netsuite-test/src/FileCabinet/SuiteScripts/test_script.js
@@ -0,0 +1,15 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType ScheduledScript
+ * @description Test script - GitHub to NetSuite deployment
+ */
+define([], function() {
+    function execute(context) {
+        log.debug({
+            title: 'GitHub Deploy Test',
+            details: 'This script was deployed from GitHub successfully!'
+        });
+    }
+
+    return { execute: execute };
+});
